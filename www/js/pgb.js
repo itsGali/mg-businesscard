81eb4d4516c07610f36337b77d95459b77c34db2
s-122-19 <s-122-19@gumiforest.ae.krakow.pl>
2017-03-14 12:34:39 +0100

    podstawa

---
+++ www/js/pgb.js
@@ -0,0 +1,34 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
+
+function userInfo() {
+
+	info =  'Your personal data:' + '\n' +
+			'=====' + '\n' +
+			'User Name    : '     + 'Mateusz' + '\n' + 
+			'User Surname : '  + 'Galant' + '\n' + 
+			'Email : '  + 'matt.galant@gmail.com'  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
